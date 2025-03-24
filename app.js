4d62f985fd951010db825305b337f53953d70d67
Ramzi Karkoub <karkoub.ramzi@gmail.com>
2025-03-23 22:34:47 -0400

    fix cors error

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(
       "https://ramzillow-gd850yjys-ramzikarkoubs-projects.vercel.app",
       "https://ramzillow-ramzikarkoubs-projects.vercel.app",
       "https://ramzillow-ramzikarkoub-ramzikarkoubs-projects.vercel.app",
+      "https://ramzillow-o0rz80xs7-ramzikarkoubs-projects.vercel.app",
     ],
     credentials: true, // Required for sending cookies
     methods: ["GET", "POST", "PUT", "DELETE"],
